6f47cf621d36dd6c543052edf8cedb1d3ff9a0a8
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-01-08 17:35:49 -0800

    final schema file

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE products (
   product_name VARCHAR(45) NOT NULL,
   department_name VARCHAR(45),
   price DECIMAL(10,2) NOT NULL,
-  quantity INT(10) NULL,
+  quantity INT(10) NOT NULL,
   PRIMARY KEY (item_id)
 );
 
