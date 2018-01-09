04fd2fc2394233a7d3de049f4d0a267fb6f79226
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-01-08 16:37:51 -0800

    displaying options in terminal

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -48,7 +48,7 @@ function initialPrompt() {
         name: "quantity",
         message: "How many units would you like to purchase?"
     }]).then(function (answer) {
-        // console.log('Customer has selected: \n    item_id = '  + input.item_id + '\n    quantity = ' + input.quantity);
+        // console.log('Customer has selected: \n    item_id = '  + answer.item_id + '\n    quantity = ' + answer.quantity);
 
         var item = answer.item_id;
         var itemQuantity = answer.quantity;
@@ -72,9 +72,44 @@ function initialPrompt() {
                     connection.query(updateQueryStr, function(err, res){
                         if (err) throw err;
                         console.log('Your total is $' + productData.price * itemQuantity);
+                        connection.end();
                     })
+                } else {
+                    console.log('Sorry, we do not currently have enough of that product. Please modify the quantity requested.')
+                    displayInventory();
                 }
             }
         })
-    });
+    })
 }
+
+function displayInventory(){
+    // console.log('test display inventory');
+    
+    queryStr = 'SELECT * FROM products';
+    connection.query(queryStr, function(err, res) {
+        if (err) throw err;
+
+        console.log('Current Inventory: \n=======================================================================================' );
+        
+        var strInventory = '';
+        for (var i = 0; i < res.length; i++) {
+            strInventory = '';
+            strInventory += 'Item ID: ' + res[i].item_id + '  ||  ';
+			strInventory += 'Product Name: ' + res[i].product_name + '  ||  ';
+			strInventory += 'Department: ' + res[i].department_name + '  ||  ';
+            strInventory += 'Price: $' + res[i].price + '\n';
+            
+            console.log(strInventory);
+        }
+        console.log('=======================================================================================' );
+
+        initialPrompt();
+    })
+}
+
+function startApp(){
+    displayInventory();
+}
+
+startApp();
